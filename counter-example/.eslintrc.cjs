8d4044044c14948e80e6ac2387518fb923fd3fcb
Jack Lingle <captainjack0818@gmail.com>
2023-07-04 15:48:56 -0400

    update eslint config and write first test

--- counter-example/.eslintrc.cjs
+++ counter-example/.eslintrc.cjs
@@ -1,10 +1,11 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, "jest/globals": true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'react-app',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
