a505580dbf338502078bc5eec0a212861ba33bd8
Jack Lingle <captainjack0818@gmail.com>
2023-07-05 11:46:58 -0400

    update eslint config for jest

--- unicafe-redux/.eslintrc.cjs
+++ unicafe-redux/.eslintrc.cjs
@@ -6,6 +6,24 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+      ],
+      env: {
+        jest: true,
+      },
+      plugins: ['jest'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
