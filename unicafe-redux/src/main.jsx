32cc31d119710db7d526178bd507998dc37ce5fd
Jack Lingle <captainjack0818@gmail.com>
2023-07-05 11:44:55 -0400

    add reducer and basic app component

--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { createStore } from 'redux';
+import reducer from './reducers/reducer';
 
+const store = createStore(reducer);
 function App () {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
 
+  return (
+    <div>
+      <button onClick={good}>Good</button>
+      <button>Ok</button>
+      <button>Bad</button>
+      <button>Reset Status</button>
+      <div>Good: {store.getState().good}</div>
+      <div>Ok: </div>
+      <div>Bad: </div>
+    </div>
+  )
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const renderApp = () => {
+  root.render(<App />);
+}
+renderApp();
+store.subcribe(renderApp);
+
