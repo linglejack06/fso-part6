458f9a787ec83d9cc98e82dae3dc4f443813f06f
Jack Lingle <captainjack0818@gmail.com>
2023-07-05 12:49:45 -0400

    add form to add note

--- counter-example/src/main.jsx
+++ counter-example/src/main.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/button-has-type */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -23,26 +25,44 @@ store.dispatch({
     id: 2,
   },
 });
+const generateId = () => Math.floor(Math.random() * 10000000000);
+
 function App() {
+  const addNote = (e) => {
+    e.preventDefault();
+    const content = e.target.note.value;
+    e.target.note.value = '';
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: {
+        id,
+      },
+    });
+  };
   return (
     <>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">Add</button>
+      </form>
       <div>
         {store.getState().map((note) => (
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content}
             <strong>{note.important ? 'important' : ''}</strong>
           </li>
         ))}
       </div>
-      <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>
-        plus
-      </button>
-      <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>
-        minus
-      </button>
-      <button onClick={() => store.dispatch({ type: 'ZERO' })}>
-        reset
-      </button>
     </>
   );
 }
