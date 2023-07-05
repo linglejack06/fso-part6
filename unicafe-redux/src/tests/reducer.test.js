0341f1f632b7f433889562a3e2eef202304bfddf
Jack Lingle <captainjack0818@gmail.com>
2023-07-05 12:39:49 -0400

    finish reducer functions

--- unicafe-redux/src/tests/reducer.test.js
+++ unicafe-redux/src/tests/reducer.test.js
@@ -8,7 +8,46 @@ describe('unicafe reducer', () => {
     bad: 0,
   };
   it ('returns initial state when called with no state', () => {
-    const state = reducer(undefined, { type: 'NONE' });
-    expect(state).toEqual(initialState);
+    const state = {};
+    deepFreeze(state);
+    const newState = reducer(undefined, { type: 'NONE' });
+    expect(newState).toEqual(initialState);
+  })
+  it ('increments good correctly', () => {
+    const state = initialState;
+    deepFreeze(state);
+    const newState = reducer(initialState, { type: 'GOOD' });
+    expect(newState).toEqual({
+      ...state,
+      good: 1,
+    })
+  })
+  it ('increments bad correctly', () => {
+    const state = initialState;
+    deepFreeze(state);
+    const newState = reducer(state, { type: 'BAD' });
+    expect(newState).toEqual({
+      ...state,
+      bad: 1,
+    })
+  })
+  it ('increments ok correctly', () => {
+    const state = initialState;
+    deepFreeze(state);
+    const newState = reducer(state, { type: 'OK' });
+    expect(newState).toEqual({
+      ...state,
+      ok: 1,
+    })
+  })
+  it ('zeroes correctly', () => {
+    const state = {
+      good: 5,
+      bad: 4,
+      ok: 9,
+    };
+    deepFreeze(state);
+    const newState = reducer(state, { type: 'ZERO' });
+    expect(newState).toEqual(initialState);
   })
 })
