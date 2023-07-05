f72649e1c08d42ea455ede4d48bdeb54a4896ab7
Jack Lingle <captainjack0818@gmail.com>
2023-07-05 16:19:35 -0400

    add basic reducer

--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import anecdotesReducer from './reducers/anecdotesReducer';
+
+const store = createStore(anecdotesReducer);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  
 )
