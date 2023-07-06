07277bded948ee464dd6d8be378a842d10f06bee
Jack Lingle <captainjack0818@gmail.com>
2023-07-06 19:59:56 -0400

    add components into app component

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,5 +1,13 @@
+import Anecdotes from './components/Anecdotes';
+import AnecdoteForm from './components/AnecdoteForm';
+
 function App() {
-  
+  return (
+    <>
+      <AnecdoteForm />
+      <Anecdotes />
+    </>
+  )
 }
 
 export default App
