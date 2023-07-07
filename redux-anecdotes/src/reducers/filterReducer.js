4703d0c5b271affcd531525c223fa74768a09ce2
Jack Lingle <captainjack0818@gmail.com>
2023-07-07 18:47:09 -0400

    refactor filter reducer to use createSlice

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,16 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 export const initialState = '';
-export const updateFilter = (value) => ({
-  type: 'UPDATE_FILTER',
-  payload: value,
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    updateFilter (state, action) {
+      const filter = action.payload;
+      return filter;
+    },
+  },
 });
 
-const filterReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'UPDATE_FILTER':
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-export default filterReducer
+export const { updateFilter } = filterSlice.actions
+export default filterSlice.reducer;
