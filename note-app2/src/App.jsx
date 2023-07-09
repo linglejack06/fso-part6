657da8d6654759a75c72c6f55ac3e7b16477bdec
Jack Lingle <captainjack0818@gmail.com>
2023-07-09 09:17:28 -0400

    initialize rewrite and add basic app component

---
+++ note-app2/src/App.jsx
@@ -0,0 +1,30 @@
+
+function App() {
+  const addNote = async (e) => {
+    e.preventDefault();
+    const content = e.target.note.value;
+    e.target.note.value = '';
+    console.log(content);
+  };
+  const toggleImportance = (note) => {
+    console.log('toggle importance of, note.id');
+  };
+  const notes = [];
+  return (
+    <>
+    <h2>Notes App Rewrite</h2>
+    <form onSubmit={addNote}>
+      <input name="note" />
+      <button type="submit">Add</button>
+    </form>
+    {notes.map((note) => (
+      <li key={note.id} onClick={() => toggleImportance(note)}>
+        {note.content}
+        <strong> {note.important ? 'important' : ''}</strong>
+      </li>
+    ))}
+    </>
+  )
+}
+
+export default App
